f9119aeac1be034bf8d9e575d2b7620c52b49b3f
nsrciog <nsrciog@gmail.com>
2022-07-28 13:00:46 +0900

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function Header () {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to={'/'}>top</Link>
+          </li>
+          <li>
+            <Link to={'/common/about'}>about</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
