de9a05a74078a3a933d5e2b9d1aab96a7dd7f910
nsrciog <nsrciog@gmail.com>
2022-07-28 13:11:56 +0900

    fix: add `<Header>` to all page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Header from '@/components/header'
 
 export default function Home() {
   return (
     <section>
-      <h1>Hello!</h1>
       <Header />
-      <Link to={'/common/about'}>About</Link>
+      <h1>Hello!</h1>
     </section>
   )
 }
