a53a4aa6d027a296e96a132fe056658969ebef12
enuesaa <nsrciog@gmail.com>
2022-12-17 17:25:31 +0900

    feat: formaを触ってみた

--- forma/src/main.rs
+++ forma/src/main.rs
@@ -1,3 +1,24 @@
+use forma::{cpu, prelude::*};
+
+/**
+ * see https://github.com/google/forma/blob/main/forma/src/cpu/buffer/mod.rs
+ */
 fn main() {
+    let mut buffer = vec![0; 4];
+    let mut composition = Composition::new();
+    let mut renderer = cpu::Renderer::new();
+    let layer_cache = renderer.create_buffer_layer_cache().unwrap();
+    renderer.render(
+        &mut composition,
+        &mut BufferBuilder::new(&mut buffer, &mut LinearLayout::new(1, 1 * 4, 1))
+            .layer_cache(layer_cache.clone())
+            .build(),
+        RGBA,
+        Color { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+        None,
+    );
+
+    assert_eq!(buffer, [255; 4]);
+
     println!("Hello, world!");
 }
